26d2e5e12d395163dbccca0bce48553b8210925a
Scott Thornton <s.thornton@lancaster.ac.uk>
2018-02-08 10:37:37 +0000

    set up hub && download the r cars json file
    set-up

---
+++ src/VehicleStat.java
@@ -0,0 +1,14 @@
+
+public class VehicleStat {
+	
+	private String SIPP, Price, Supplier, Name, Rating;
+	
+	public VehicleStat(String SIPP,String Price,String Supplier,String Name,String Rating) {
+		this.SIPP 		= SIPP;
+		this.Price 		= Price;
+		this.Supplier 	= Supplier;
+		this.Name 		= Name;
+		this.Rating 	= Rating;
+	}
+	
+}
